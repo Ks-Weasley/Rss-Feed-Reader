a85385594ad795840ba4de8a4be3d862ade26530
Ks-Weasley <krithikaswami02@gmail.com>
2020-04-30 15:51:01 +0530

    third update

--- lib/webview.dart
+++ lib/webview.dart
@@ -1,46 +1,75 @@
 // navigates to a new page internal to the app when chosen from the list tile
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-
 
 class WebViewContainer extends StatefulWidget {
   final url;
   final bool press;
+
   WebViewContainer(this.url, this.press);
+
   @override
   createState() => _WebViewContainerState(this.url, this.press, this.press);
 }
+
 class _WebViewContainerState extends State<WebViewContainer> {
   var _url;
   bool press, presst;
   final _key = UniqueKey();
+  bool _isLoading;
+  Completer<WebViewController> _controller = Completer<WebViewController>();
 
   _WebViewContainerState(this._url, this.press, this.presst);
-  changeState(){
-    setState(()=> press=!presst);
+
+  void initState() {
+    super.initState();
+    _isLoading = true;
   }
+
+  changeState() {
+    setState(() => press = !presst);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          leading: IconButton(icon: Icon(Icons.arrow_back), onPressed: ()=> Navigator.pop(context, press!= presst),),
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () => Navigator.pop(context, press != presst),
+          ),
           actions: <Widget>[
             IconButton(
-              icon: (press)?Icon(Icons.favorite): Icon(Icons.favorite_border ),
+              icon:
+              (press) ? Icon(Icons.favorite) : Icon(Icons.favorite_border),
               color: Colors.red,
-              onPressed: (){ changeState();
-          },),],
+              onPressed: () {
+                changeState();
+              },
+            ),
+          ],
         ),
-        body: Column(
-          children: [
-            Expanded(
-                child: WebView(
-                    key: _key,
-                    javascriptMode: JavascriptMode.unrestricted,
-                    initialUrl: _url))
-          ,]
-
-        )
-    , );
+        body: Stack(
+          children: <Widget>[
+            WebView(
+              key: _key,
+              javascriptMode: JavascriptMode.unrestricted,
+              initialUrl: _url,
+              onWebViewCreated: (webViewCreated) {
+                _controller.complete(webViewCreated);
+              },
+              onPageFinished: (finish) {
+                setState(() {
+                  _isLoading = false;
+                });
+              },
+            ),
+            _isLoading
+                ? Center(child: CircularProgressIndicator())
+                : Container()
+          ],
+        ));
   }
 }
